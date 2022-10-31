eaa3b768b299ab610c3c1c9d0aca1f51a0f2931b
[jew6] <[jew6@usca.edu]>
2022-10-31 15:39:24 -0400

    Jonathan Walker Halloween update

---
+++ js/changeLink.js
@@ -0,0 +1,11 @@
+/* try JQuery */
+$(function(){//document.ready.....
+
+    $("a").each(function(){
+        $(this).css("text-decoration","line-through");
+        $(this).attr('href',"https://www.youtube.com");
+});
+
+
+
+});
